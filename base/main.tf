500d0030ef0b648f87b7ebc2df2f04f6d711ac3d
Mark Tinderholt <mark_tinderholt@persistent.co.in>
2020-06-02 14:23:12 -0400

    initial resoruce group

---
+++ base/main.tf
@@ -0,0 +1,12 @@
+
+resource "azurerm_resource_group" "rg" {
+    
+  name      = var.name
+  location  = var.location
+
+  tags {
+    app = var.app_name
+    env = var.env_name
+  }
+
+}
---
+++ base/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {}
+variable "location" {}
+variable "app_name" {}
+variable "env_name" {}
